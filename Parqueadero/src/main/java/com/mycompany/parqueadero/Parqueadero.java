ea6eecdf61bae48a8e47e13119803ca57ff77278
danielfe2005 <daniel.vargas1@estudiantesunibague.edu.co>
2026-02-22 19:08:57 -0500

    interfaz parqueadero

---
+++ Parqueadero/src/main/java/com/mycompany/parqueadero/Parqueadero.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.parqueadero;
+
+/**
+ *
+ * @author DANIEL
+ */
+public class Parqueadero {
+
+    public static void main(String[] args) {
+        new interfaz.EmisorUI().setVisible(true);
+    }
+}
